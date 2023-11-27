1f36d9428dc75806c3d5370e4a7fa4105300c609
djk1tty <Daniil.Kolokoltsev@mail.ru>
2023-11-27 17:59:30 +0300

    Initial commit

---
+++ src/Main2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main2 {
+
+    public static void main(String[] args) {
+        int maxRowNum = 10;
+        int maxColumnNum = 10;
+
+        for (int row = 1; row <= maxRowNum; row++) {
+            for (int column = 1; column <= maxColumnNum; column++) {
+                System.out.print(row * column + " ");
+            }
+            System.out.println();
+        }
+    }
+}
